logstash-plugins/logstash-filter-translate

optionally use event.sprintf on dictitionary values

lib/logstash/filters/translate.rb
@@ -150,6 +150,9 @@ class Translate < LogStash::Filters::Base
   # the field to write the looked up value (or the `fallback` value) to with `destination`
   config :iterate_on, :validate => :string
 
+  # If you'd like dictionary values to be evaluated dynamically set `dynamic => true`.
+  config :dynamic, :validate => :boolean, :default => false
+
   attr_reader :lookup # for testing reloading
 
   def register
@@ -168,11 +171,11 @@ class Translate < LogStash::Filters::Base
       @lookup = Dictionary::Memory.new(@dictionary, @exact, @regex)
     end
     if @iterate_on.nil?
-      @updater = SingleValueUpdate.new(@field, @destination, @fallback, @lookup)
+      @updater = SingleValueUpdate.new(@field, @destination, @fallback, @lookup, @dynamic)
     elsif @iterate_on == @field
-      @updater = ArrayOfValuesUpdate.new(@iterate_on, @destination, @fallback, @lookup)
+      @updater = ArrayOfValuesUpdate.new(@iterate_on, @destination, @fallback, @lookup, @dynamic)
     else
-      @updater = ArrayOfMapsValueUpdate.new(@iterate_on, @field, @destination, @fallback, @lookup)
+      @updater = ArrayOfMapsValueUpdate.new(@iterate_on, @field, @destination, @fallback, @lookup, @dynamic)
     end
 
     @logger.debug? && @logger.debug("#{self.class.name}: Dictionary - ", :dictionary => @lookup.dictionary)

spec/filters/translate_spec.rb
@@ -499,4 +499,54 @@ describe LogStash::Filters::Translate do
       end
     end
   end
+
+  describe "dnynamic translation" do
+
+    let(:config) do
+      {
+        "field"       => "status",
+        "destination" => "translation",
+        "dictionary"  => [ "200", "O%{[other]}K",
+                           "300", "Redirect",
+                           "400", "Client Error",
+                           "500", "Server Error" ],
+        "exact"       => true,
+        "regex"       => false,
+        "dynamic"     => true
+      }
+    end
+
+    let(:event) { LogStash::Event.new("status" => 200, "other" => "test123") }
+
+    it "coerces field to a string then returns the exact translation" do
+      subject.register
+      subject.filter(event)
+      expect(event.get("translation")).to eq("Otest123K")
+    end
+  end
+
+  describe "non-dynamic translation" do
+
+    let(:config) do
+      {
+        "field"       => "status",
+        "destination" => "translation",
+        "dictionary"  => [ "200", "OK",
+                           "300", "Redirect",
+                           "400", "Client Error",
+                           "500", "Server Error",
+                           "testdyn", "some%{[other]}value" ],
+        "exact"       => true,
+        "regex"       => false
+      }
+    end
+
+    let(:event) { LogStash::Event.new("status" => "testdyn", "other" => "test123") }
+
+    it "won't use dynamic evaluation when disabled" do
+      subject.register
+      subject.filter(event)
+      expect(event.get("translation")).to eq("some%{[other]}value")
+    end
+  end
 end

fholzer/logstash-filter-dyntranslate

lib/logstash/filters/translate.rb
@@ -167,6 +167,9 @@ class Translate < LogStash::Filters::Base
   # the field to write the looked up value (or the `fallback` value) to with `destination`
   config :iterate_on, :validate => :string
 
+  # If you'd like dictionary values to be evaluated dynamically set `dynamic => true`.
+  config :dynamic, :validate => :boolean, :default => false
+
   attr_reader :lookup # for testing reloading
   attr_reader :updater # for tests
 
@@ -218,11 +221,11 @@ class Translate < LogStash::Filters::Base
     end
 
     if @iterate_on.nil?
-      @updater = SingleValueUpdate.new(@source, @target, @fallback, @lookup)
+      @updater = SingleValueUpdate.new(@source, @target, @fallback, @lookup, @dynamic)
     elsif @iterate_on == @source
-      @updater = ArrayOfValuesUpdate.new(@iterate_on, @target, @fallback, @lookup)
+      @updater = ArrayOfValuesUpdate.new(@iterate_on, @target, @fallback, @lookup, @dynamic)
     else
-      @updater = ArrayOfMapsValueUpdate.new(@iterate_on, @source, @target, @fallback, @lookup)
+      @updater = ArrayOfMapsValueUpdate.new(@iterate_on, @source, @target, @fallback, @lookup, @dynamic)
     end
 
     @logger.debug? && @logger.debug("#{self.class.name}: Dictionary - ", :dictionary => @lookup.dictionary)

spec/filters/translate_spec.rb
@@ -654,4 +654,53 @@ describe LogStash::Filters::Translate do
 
   end
 
+  describe "dnynamic translation" do
+
+    let(:config) do
+      {
+        "field"       => "status",
+        "target"      => "translation",
+        "dictionary"  => [ "200", "O%{[other]}K",
+                           "300", "Redirect",
+                           "400", "Client Error",
+                           "500", "Server Error" ],
+        "exact"       => true,
+        "regex"       => false,
+        "dynamic"     => true
+      }
+    end
+
+    let(:event) { LogStash::Event.new("status" => 200, "other" => "test123") }
+
+    it "coerces field to a string then returns the exact translation" do
+      subject.register
+      subject.filter(event)
+      expect(event.get("translation")).to eq("Otest123K")
+    end
+  end
+
+  describe "non-dynamic translation" do
+
+    let(:config) do
+      {
+        "field"       => "status",
+        "target"      => "translation",
+        "dictionary"  => [ "200", "OK",
+                           "300", "Redirect",
+                           "400", "Client Error",
+                           "500", "Server Error",
+                           "testdyn", "some%{[other]}value" ],
+        "exact"       => true,
+        "regex"       => false
+      }
+    end
+
+    let(:event) { LogStash::Event.new("status" => "testdyn", "other" => "test123") }
+
+    it "won't use dynamic evaluation when disabled" do
+      subject.register
+      subject.filter(event)
+      expect(event.get("translation")).to eq("some%{[other]}value")
+    end
+  end
 end